bb2aef1374749c4790bd1afc9563f7df7dac6c1e
akshaybankar007 <akshaybankar076@gmail.com>
2026-05-31 13:25:57 +0530

    DOM Loading and user message sending feature

--- public/app.js
+++ public/app.js
@@ -1,5 +1,31 @@
-// Caching core DOM components
-const mainInput = document.querySelector('#mainInput');
-const sendBtn = document.querySelector('#sendBtn');
-const chatContainer = document.querySelector('#chatContainer');
-const messageStream = document.querySelector('#messageStream');
+document.addEventListener('DOMContentLoaded', () => {
+
+    let isChatActive = false;
+
+    const mainInput = document.querySelector('#mainInput');
+    const sendBtn = document.querySelector('#sendBtn');
+    const chatContainer = document.querySelector('#chatContainer');
+    const messageStream = document.querySelector('#messageStream');
+    const landingView = document.querySelector('#landingView');
+    const newChatBtn = document.querySelector('#newChatBtn');
+
+
+    function activateChat() {
+        landingView.style.display = 'none';
+        messageStream.style.display = 'flex';
+        isChatActive = true;
+    }
+
+    function handleSend(){
+    const userInput = mainInput.value;
+    activateChat();
+    const div = document.createElement('div');
+    div.textContent = userInput;
+    div.classList.add('message');
+    div.classList.add('user-message');
+    messageStream.appendChild(div);
+
+    mainInput.value = '';   
+ }
+    sendBtn.addEventListener('click', handleSend);
+});
