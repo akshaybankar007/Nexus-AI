d5746ad9109562eb84bc05648a6cd09cd42cefa2
akshaybankar007 <akshaybankar076@gmail.com>
2026-05-31 14:07:24 +0530

    Chat history and Enter Key feature

--- public/app.js
+++ public/app.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let isChatActive = false;
 
+    let chatHistory = [];
+
     const mainInput = document.querySelector('#mainInput');
     const sendBtn = document.querySelector('#sendBtn');
     const chatContainer = document.querySelector('#chatContainer');
@@ -18,14 +20,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function handleSend(){
     const userInput = mainInput.value;
+    chatHistory.push(userInput);
+
     activateChat();
+
     const div = document.createElement('div');
     div.textContent = userInput;
-    div.classList.add('message');
-    div.classList.add('user-message');
+    div.classList.add('message', 'user-message');
     messageStream.appendChild(div);
 
     mainInput.value = '';   
  }
     sendBtn.addEventListener('click', handleSend);
+
+    mainInput.addEventListener('keydown', (event) =>{
+    if(event.key === 'Enter'){
+        handleSend();
+    }
+})
 });
+
